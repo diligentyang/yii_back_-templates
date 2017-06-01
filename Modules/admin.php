58e5f2e6d6b56b1638855345159070c8fd34ea71
diligentyang <905407339@qq.com>
2017-06-01 16:19:47 +0800

    init

---
+++ Modules/admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\Modules;
+
+/**
+ * admin module definition class
+ */
+class admin extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\Modules\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
