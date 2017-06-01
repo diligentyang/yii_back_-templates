230f8368eed6d2897dcebd3080d7acd6a49bc735
diligentyang <905407339@qq.com>
2017-06-01 21:09:21 +0800

    登录

--- modules/models/Admin.php
+++ modules/models/Admin.php
@@ -8,24 +8,51 @@ class Admin extends ActiveRecord
 {
 	public $rememberMe = true;
 	
-	public function rules()
-	{
-		return [
-			['adminuser','required','message'=>'管理员账号不能为空','on'=>'login'],
-			['adminpass','required','message'=>'管理员密码不能为空','on'=>'login'],
-		];
-	}
-	
 	public static function tableName()
     {
         return "{{%admin}}";
     }
 	
+	public function rules()
+	{
+		return [
+			['adminuser','required','message'=>'管理员账号不能为空','on'=>'login'],
+			['adminpass','required','message'=>'管理员密码不能为空','on'=>'login'],
+			['rememberMe', 'boolean', 'on' => 'login'],
+			['adminpass','validatePass','on'=>'login'],
+		];
+	}
+	
+	public function validatePass()
+	{
+		if(!$this->hasErrors()){
+			$data = self::find()->where('adminuser=:user and adminpass=:pass',[":user"=>$this->adminuser,":pass"=>md5($this->adminpass)])->one();
+			if(is_null($data)){
+				$this->addError("adminpass", "用户名或者密码错误");
+			}
+			
+		}
+	}
+	
 	public function login($data){
 		$this->scenario = "login";
 		if($this->load($data) && $this->validate()){
-			echo "222222222";
-			return true;
+			 //做点有意义的事
+            $lifetime = $this->rememberMe ? 24*3600 : 0;
+            $session = Yii::$app->session;
+            session_set_cookie_params($lifetime);//设置保存时间
+            $session['admin'] = [
+                'adminuser' => $this->adminuser,
+                'isLogin' => 1,
+            ];
+			
+			$loginip = Yii::$app->request->userIP;
+			if($loginip == "::1"){
+                $loginip = "127.0.0.1";
+            }
+            $loginip = ip2long($loginip);
+			$this->updateAll(['logintime' => time(), 'loginip' => $loginip], 'adminuser = :user', [':user' => $this->adminuser]);
+			return (bool)$session['admin']['isLogin'];
 		}
 		return false;
 	}
