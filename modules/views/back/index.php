423e4df5a298f5130ee07567789e74bbfdfeb038
diligentyang <905407339@qq.com>
2017-06-02 19:05:57 +0800

    update back index

--- modules/views/back/index.php
+++ modules/views/back/index.php
@@ -1,1 +1,8 @@
+<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="<?php echo yii\helpers\Url::to(['back/index']);?>"><span class="glyphicon glyphicon-home"></span></a></li>
+				<li class="active">首页</li>
+			</ol>
+</div><!--/.row-->
+
 首页
