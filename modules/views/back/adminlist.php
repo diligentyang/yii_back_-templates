40673c0ab40e456810eabc431434db2bdf81e624
diligentyang <905407339@qq.com>
2017-06-04 14:37:28 +0800

    update adminlist

--- modules/views/back/adminlist.php
+++ modules/views/back/adminlist.php
@@ -5,4 +5,51 @@
 			</ol>
 </div><!--/.row-->
 
-这是管理员管理界面
+<div class="row">
+	<div class="col-md-12" style="height:80px;">
+		<h3 class="pull-left">管理员列表</h3>
+		<button type="button" class="btn btn-success pull-right" style="margin-top:20px;">添加管理员</button>
+	</div>
+</div>
+<table class="table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>管理员账号</th>
+          <th>管理员邮箱</th>
+          <th>最后登录时间</th>
+		  <th>最后登录IP</th>
+		  <th>添加时间</th>
+		  <th>操作</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>Thornton</td>
+          <td>Thornton</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>the Bird</td>
+          <td>the Bird</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
